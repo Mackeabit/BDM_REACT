3ef738d8370dc33d741e92ecf0e6616fce8fcc51
Mackeabit <thdrudgh7775@naver.com>
2023-09-11 22:41:02 +0900

    줌레벨 최소, 최대값 수정 및 표출 범위 수정

--- bdo-map/src/components/MapComponent.js
+++ bdo-map/src/components/MapComponent.js
@@ -3,13 +3,27 @@ import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const MapComponent = () => {
-    const centerPosition = [70, -80]; // 지도의 중심 위치. 필요에 따라 조정하세요.
+    const centerPosition = [70, -80];
+    const maxBounds = [
+        [-90, -180], // 왼쪽 하단 좌표
+        [190, 180]  // 오른쪽 상단 좌표
+    ];
 
     return (
-        <MapContainer center={centerPosition} zoom={4} style={{ width: '100%', height: '600px' }}>
+        <MapContainer 
+            center={centerPosition} 
+            zoom={2} 
+            minZoom={2} 
+            maxZoom={6} 
+            maxBounds={maxBounds} 
+            worldCopyJump={false}
+            bounceAtZoomLimits={false}
+            style={{ width: '100vw', height: '100vh' }}
+        >
             <TileLayer
                 url="http://localhost:8389/tiles/{z}/{x}/{y}.png"
                 attribution="&copy; 검은사막 지도"
+                noWrap={true}
             />
         </MapContainer>
     );
