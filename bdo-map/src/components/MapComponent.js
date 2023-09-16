fb7aa223c55f43c12aecee3ea397ea98c56386fe
Mackeabit <thdrudgh7775@naver.com>
2023-09-16 23:50:52 +0900

    버튼 클릭시 해당 마커  데이터 로드 기능 추가

--- bdo-map/src/components/MapComponent.js
+++ bdo-map/src/components/MapComponent.js
@@ -1,97 +1,80 @@
 import React, { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Tooltip } from 'react-leaflet';
+import axios from 'axios';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import npcIcon from '../assets/icons/npc/npc_icon.png';
 
-// 아이콘 이미지 경로 설정
 const icon = new L.Icon({
-    iconUrl: npcIcon,
-    iconSize: [32, 32],
+  iconUrl: npcIcon,
+  iconSize: [32, 32],
 });
 
 const MapComponent = () => {
-    const centerPosition = [70, -80];
-    const maxBounds = [
-        [-90, -180],
-        [190, 180]
-    ];
+  const centerPosition = [70, -80];
+  const maxBounds = [
+    [-90, -180],
+    [190, 180]
+  ];
 
-    // 마커 표시 상태
-    const [showNPC, setShowNPC] = useState(false);
-    const [showPosition, setShowPosition] = useState(false);
+  const [npcs, setNpcs] = useState([]);
+  const [locations, setLocations] = useState([]);
 
-    // 예시 NPC 데이터
-    const npcs = [
-        {
-            name: "General Vendor",
-            position: [78, -75]
-        },
-        {
-            name: "Skill Instructor",
-            position: [79, -76]
-        }
-    ];
+  const fetchNpcs = async () => {
+    try {
+      const response = await axios.get('http://localhost:8389/api/npcs');
+      setNpcs(response.data);
+    } catch (error) {
+      console.error("No NPCs found:", error);
+    }
+  };
 
-    // 예시 거점 데이터
-    const positions = [
-        {
-            name: "House 1",
-            position: [80, -80],
-            icon: "position1.png"
-        },
-        {
-            name: "House 2",
-            position: [81, -82],
-            icon: "position2.png"
-        }
-    ];
+  const fetchLocations = async () => {
+    try {
+      const response = await axios.get('http://localhost:8389/api/locations');
+      setLocations(response.data);
+    } catch (error) {
+      console.error("No Locations found:", error);
+    }
+  };
 
-    return (
-        <div>
-            <button onClick={() => setShowNPC(!showNPC)}>
-                {showNPC ? 'Hide' : 'Show'} NPC
-            </button>
-            <button onClick={() => setShowPosition(!showPosition)}>
-                {showPosition ? 'Hide' : 'Show'} Position
-            </button>
-            
-            <MapContainer 
-                center={centerPosition} 
-                zoom={2} 
-                minZoom={2} 
-                maxZoom={6} 
-                maxBounds={maxBounds} 
-                worldCopyJump={false}
-                bounceAtZoomLimits={false}
-                style={{ width: '100vw', height: '100vh' }}
-            >
-                <TileLayer
-                    url="http://localhost:8389/tiles/{z}/{x}/{y}.png"
-                    attribution="&copy; 검은사막 지도"
-                    noWrap={true}
-                />
+  return (
+    <div>
+      <button onClick={fetchNpcs}>
+        Show NPCs
+      </button>
+      <button onClick={fetchLocations}>
+        Show Locations
+      </button>
 
-                {showNPC && npcs.map((npc, index) => (
-                    <Marker key={index} position={npc.position} icon={icon}>
-                        <Tooltip>{npc.name}</Tooltip>
-                    </Marker>
-                ))}
+      <MapContainer 
+        center={centerPosition} 
+        zoom={2} 
+        minZoom={2} 
+        maxZoom={6} 
+        maxBounds={maxBounds}
+        style={{ width: '100vw', height: '100vh' }}
+      >
+        <TileLayer
+          url="http://localhost:8389/tiles/{z}/{x}/{y}.png"
+          attribution="&copy; 검은사막 지도"
+          noWrap={true}
+        />
 
-                {showPosition && positions.map((pos, index) => {
-                    const positionIcon = new L.Icon({
-                        iconUrl: `http://localhost:8389/public/icons/points/${pos.icon}`,
-                        iconSize: [32, 32],
-                    });
-                    return (
-                        <Marker key={index} position={pos.position} icon={positionIcon}>
-                            <Tooltip>{pos.name}</Tooltip>
-                        </Marker>
-                    );
-                })}
-            </MapContainer>
-        </div>
-    );
-}
+        {npcs.map((npc, index) => (
+          <Marker key={index} position={npc.position} icon={icon}>
+            <Tooltip>{npc.name}</Tooltip>
+          </Marker>
+        ))}
+
+        {locations.map((location, index) => (
+          <Marker key={index} position={location.position}>
+            <Tooltip>{location.name}</Tooltip>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
+  );
+};
 
 export default MapComponent;
