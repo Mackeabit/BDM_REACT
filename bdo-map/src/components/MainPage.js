79f0416746688b6e8d876b15e83da71953f2a23b
Mackeabit <thdrudgh7775@naver.com>
2023-10-06 21:10:51 +0900

    마킹 테두리 오류 수정

--- bdo-map/src/components/MainPage.js
+++ bdo-map/src/components/MainPage.js
@@ -16,14 +16,12 @@ const MainPage = () => {
   const navigate = useNavigate();
 
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedMarker, setSelectedMarker] = useState(markerIcon1);
+  const [selectedMarkerIndex, setSelectedMarkerIndex] = useState(null);  // 초기값을 null로 설정
 
-  // 선택된 마커에 대한 스타일
   const selectedStyle = {
     border: '2px solid blue',
     borderRadius: '5px'
   };
-
 
   const handleLogout = () => {
     logout();
@@ -39,10 +37,13 @@ const MainPage = () => {
 
   const closeModal = () => {
     setIsModalOpen(false);
+    resetCursor();
+    setSelectedMarkerIndex(null);  // 선택된 마커 인덱스 초기화
   };
 
-  const setCursorToMarker = (marker) => {
+  const setCursorToMarker = (marker, index) => {
     document.body.style.cursor = `url(${marker}) 25 25, auto`;
+    setSelectedMarkerIndex(index); // 선택된 마커의 인덱스를 상태에 저장
   };
 
   const resetCursor = () => {
@@ -54,7 +55,7 @@ const MainPage = () => {
       zIndex: 1000, 
     },
     overlay: {
-        zIndex: 999,
+      zIndex: 999,
     }
   };
 
@@ -73,35 +74,36 @@ const MainPage = () => {
         <button onClick={goToLogin}>로그인</button>
       )}
 
-        <Modal 
-          isOpen={isModalOpen}
-          onRequestClose={closeModal}
-          contentLabel="Mark Modal"
-          style={customStyles}
-        >
-          <h2>마킹하기</h2>
-          <div>
-            <button 
-              onClick={() => setCursorToMarker(markerIcon1)}
-              style={selectedMarker === markerIcon1 ? selectedStyle : {}}
-            >
-                <img src={markerIcon1} alt="Marker 1" width="50" />
-            </button>
-            <button 
-              onClick={() => setCursorToMarker(markerIcon2)}
-              style={selectedMarker === markerIcon2 ? selectedStyle : {}}
-            >
-                <img src={markerIcon2} alt="Marker 2" width="50" />
-            </button>
-            <button 
-              onClick={() => setCursorToMarker(markerIcon3)}
-              style={selectedMarker === markerIcon3 ? selectedStyle : {}}
-            >
-                <img src={markerIcon3} alt="Marker 3" width="50" />
-            </button>
-          </div>
-          <button onClick={() => { closeModal(); resetCursor(); }}>닫기</button>
-        </Modal>
+      <Modal 
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="Mark Modal"
+        style={customStyles}
+      >
+        <h2>마킹하기</h2>
+        <div>
+          <button 
+            onClick={() => setCursorToMarker(markerIcon1, 1)}
+            style={selectedMarkerIndex === 1 ? selectedStyle : {}}
+          >
+              <img src={markerIcon1} alt="Marker 1" width="50" />
+          </button>
+          <button 
+            onClick={() => setCursorToMarker(markerIcon2, 2)}
+            style={selectedMarkerIndex === 2 ? selectedStyle : {}}
+          >
+              <img src={markerIcon2} alt="Marker 2" width="50" />
+          </button>
+          <button 
+            onClick={() => setCursorToMarker(markerIcon3, 3)}
+            style={selectedMarkerIndex === 3 ? selectedStyle : {}}
+          >
+              <img src={markerIcon3} alt="Marker 3" width="50" />
+          </button>
+        </div>
+        <button onClick={closeModal}>닫기</button>
+      </Modal>
+
       <MapComponent />
     </div>
   );
