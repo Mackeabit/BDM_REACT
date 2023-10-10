3a75572e9cf55d76f5880501c4a83b2e7bf607c1
Mackeabit <thdrudgh7775@naver.com>
2023-10-10 21:01:04 +0900

    지도 클릭시 modal창 입력상자 뜨게 변경

--- bdo-map/src/components/MapComponent.js
+++ bdo-map/src/components/MapComponent.js
@@ -19,6 +19,8 @@ const MapComponent = ({ cursorStyle }) => {
 
   const [npcs, setNpcs] = useState([]);
   const [locations, setLocations] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [clickedPosition, setClickedPosition] = useState(null);
 
   useEffect(() => {
     const mapRoot = document.querySelector(".leaflet-container");
@@ -27,38 +29,77 @@ const MapComponent = ({ cursorStyle }) => {
     }
   }, [cursorStyle]);
 
-  const fetchNpcs = async () => {
-    try {
-      const response = await axios.get('http://localhost:8389/api/npcs');
-      setNpcs(response.data);
-    } catch (error) {
-      console.error("No NPCs found:", error);
-    }
-  };
-
-  const fetchLocations = async () => {
-    try {
-      const response = await axios.get('http://localhost:8389/api/locations');
-      setLocations(response.data);
-    } catch (error) {
-      console.error("No Locations found:", error);
-    }
-  };
-
   const MapClickHandler = () => {
     useMapEvent('click', (e) => {
       const { lat, lng } = e.latlng;
-      alert(`클릭한 위치의 좌표: (${lat.toFixed(4)}, ${lng.toFixed(4)})`);
+      setClickedPosition([lat.toFixed(4), lng.toFixed(4)]);
+      setShowModal(true);
     });
-    return null; // 컴포넌트가 실제로 렌더링 되지 않게 null 반환
+    return null;
+  };
+
+  const Modal = ({ onClose, position }) => {
+    return (
+      <div style={modalStyle.overlay}>
+        <div style={modalStyle.modal}>
+          <h2>위치: ({position[0]}, {position[1]})</h2>
+          <label>
+            카테고리명:
+            <input type="text" />
+          </label>
+          <br />
+          <label>
+            설명:
+            <textarea />
+          </label>
+          <br />
+          <button onClick={onClose}>닫기</button>
+          <button>저장하기</button>
+        </div>
+      </div>
+    );
+  };
+
+  const modalStyle = {
+    overlay: {
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      backgroundColor: 'rgba(0, 0, 0, 0.7)',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      zIndex: 10001
+    },
+    modal: {
+      background: 'white',
+      borderRadius: '5px',
+      padding: '20px',
+      width: '300px',
+      boxShadow: '0px 0px 10px 2px rgba(0,0,0,0.2)'
+    }
   };
 
   return (
     <div style={{ position: 'relative', zIndex: 10 }}>
-      <button onClick={fetchNpcs}>
+      <button onClick={() => {
+        axios.get('http://localhost:8389/api/npcs').then(response => {
+          setNpcs(response.data);
+        }).catch(error => {
+          console.error("No NPCs found:", error);
+        });
+      }}>
         Show NPCs
       </button>
-      <button onClick={fetchLocations}>
+      <button onClick={() => {
+        axios.get('http://localhost:8389/api/locations').then(response => {
+          setLocations(response.data);
+        }).catch(error => {
+          console.error("No Locations found:", error);
+        });
+      }}>
         Show Locations
       </button>
 
@@ -90,6 +131,13 @@ const MapComponent = ({ cursorStyle }) => {
           </Marker>
         ))}
       </MapContainer>
+
+      {showModal && 
+        <Modal 
+          onClose={() => setShowModal(false)} 
+          position={clickedPosition} 
+        />
+      }
     </div>
   );
 };
