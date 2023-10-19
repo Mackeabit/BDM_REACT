99450aebb7fd6502a0d2b972570e5e52dc299b45
Mackeabit <thdrudgh7775@naver.com>
2023-10-19 23:18:42 +0900

    카테고리 css 수정 및 헤더바 추가

--- bdo-map/src/components/MainPage.js
+++ bdo-map/src/components/MainPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import MapComponent from './MapComponent';
@@ -6,8 +6,90 @@ import markerIcon1 from '../assets/icons/npc/npc_icon.png';
 import markerIcon2 from '../assets/icons/npc/npc_icon.png';
 import markerIcon3 from '../assets/icons/npc/npc_icon.png';
 import menuIcon from '../assets/icons/menuIcon.svg';
+import { motion } from 'framer-motion';
 
-const Sidebar = ({ isOpen }) => {
+const HeaderBar = ({ toggleSidebar }) => {
+  const { auth } = useAuth();
+  
+  const [shadow, setShadow] = useState(false);
+  const [hasScrolled, setHasScrolled] = useState(false);  // 스크롤이 처음으로 움직였는지 여부를 추적하는 상태
+  const [isTransparent, setIsTransparent] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        if (!hasScrolled) {
+          setHasScrolled(true);
+          setShadow(true);
+          setTimeout(() => setShadow(false), 200);
+        }
+        setIsTransparent(true);  // 스크롤이 움직이면 투명도를 활성화
+      } else {
+        setIsTransparent(false);  // 스크롤이 최상단에 있으면 투명도를 비활성화
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [hasScrolled]);
+
+  const headerVariants = {
+    hidden: {
+      opacity: 0,
+      y: '-100%'
+    },
+    visible: {
+      opacity: 1,
+      y: '0',
+      transition: {
+        type: 'spring',
+        stiffness: 100,
+        damping: 20
+      }
+    }
+  };
+
+  return (
+    <motion.div 
+      initial="hidden"
+      animate="visible"
+      variants={headerVariants}
+      style={{
+        position: 'fixed',
+        top: '1%', // 중앙에 위치하기 위해 조정
+        left: '25%', // 중앙에 위치하기 위해 조정
+        width: '50%', // 너비를 50%로 줄임
+        height: '30px', // 높이를 늘림
+        background: isTransparent ? 'rgba(51, 51, 51, 0.7)' : '#333',
+        padding: '10px 20px',
+        zIndex: 100002,
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderBottom: '1px solid #444',
+        color: 'white',
+        borderRadius: '10px', // 모서리를 둥글게 함
+        boxShadow: shadow ? '0px 4px 20px rgba(255, 255, 255, 0.8)' : 'none'  // 조건에 따라 그림자 효과를 적용
+      }}
+    >
+      <div>
+        <img 
+          src={menuIcon} 
+          alt="Open Sidebar" 
+          onClick={toggleSidebar} 
+          style={{ cursor: 'pointer' }}
+        />
+      </div>
+      <div>
+        {auth.isAuthenticated ? (
+          <span>Login {auth.user.name} OK</span>
+        ) : null}
+      </div>
+    </motion.div>
+  );
+};
+
+const Sidebar = ({ isOpen, toggleSidebar }) => {
   const [showExtraButtons, setShowExtraButtons] = useState(false);
 
   const sidebarStyle = {
@@ -36,10 +118,6 @@ const Sidebar = ({ isOpen }) => {
     transition: 'background 0.3s'
   };
 
-  const buttonHoverStyle = {
-    background: '#444'
-  };
-
   const buttonFadeInStyle = {
     ...buttonStyle,
     opacity: showExtraButtons ? 1 : 0,
@@ -49,6 +127,14 @@ const Sidebar = ({ isOpen }) => {
 
   return (
     <div style={sidebarStyle}>
+      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <img 
+          src={menuIcon} 
+          alt="Close Sidebar" 
+          onClick={toggleSidebar} 
+          style={{ cursor: 'pointer', margin: '10px' }}
+        />
+      </div>
       <h2>Sidebar</h2>
       <ul>
         <li style={listItemStyle}>Home</li>
@@ -58,9 +144,9 @@ const Sidebar = ({ isOpen }) => {
       <div style={{ position: 'absolute', bottom: '10px', width: '90%', left: '5%' }}>
         {showExtraButtons && (
           <>
-            <button style={buttonFadeInStyle} onMouseEnter={() => buttonHoverStyle} onMouseLeave={() => buttonStyle}>Button 1</button>
-            <button style={buttonFadeInStyle} onMouseEnter={() => buttonHoverStyle} onMouseLeave={() => buttonStyle}>Button 2</button>
-            <button style={buttonFadeInStyle} onMouseEnter={() => buttonHoverStyle} onMouseLeave={() => buttonStyle}>Button 3</button>
+            <button style={buttonFadeInStyle}>Button 1</button>
+            <button style={buttonFadeInStyle}>Button 2</button>
+            <button style={buttonFadeInStyle}>Button 3</button>
           </>
         )}
       </div>
@@ -74,7 +160,19 @@ const MainPage = () => {
 
   const [showMarkersDropdown, setShowMarkersDropdown] = useState(false);
   const [selectedMarkerIndex, setSelectedMarkerIndex] = useState(null);
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);  // Sidebar state
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [showConsole, setShowConsole] = useState(false);
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.ctrlKey && e.key === '`') {
+        setShowConsole(prev => !prev);
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, []);
 
   const selectedStyle = {
     border: '2px solid blue',
@@ -99,30 +197,13 @@ const MainPage = () => {
     setShowMarkersDropdown(false);
   };
 
-  const menuIconStyle = {
-    position: 'absolute',
-    top: '10px',
-    left: isSidebarOpen ? '210px' : '10px',
-    transition: '0.3s',
-    cursor: 'pointer',
-    zIndex: 100001
-  };
-
   return (
     <div>
-      <img 
-        src={menuIcon} 
-        alt="Menu Icon" 
-        onClick={() => setIsSidebarOpen(!isSidebarOpen)} 
-        style={menuIconStyle}
-      />
-      <Sidebar isOpen={isSidebarOpen} />
+      <HeaderBar toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} />
+      <Sidebar isOpen={isSidebarOpen} toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} />
       <h1 style={{textAlign:'center'}}>검은사막 지도</h1>
       {auth.isAuthenticated ? (
         <div>
-          <span style={{ color: 'green' }}>
-            Login {auth.user.name} OK
-          </span>
           <button onClick={handleLogout}>로그아웃</button>
           <button onClick={toggleMarkersDropdown}>마킹하기</button>
           {showMarkersDropdown && (
@@ -160,6 +241,32 @@ const MainPage = () => {
         <button onClick={goToLogin}>로그인</button>
       )}
       <MapComponent cursorStyle={document.body.style.cursor} isAuthenticated={auth.isAuthenticated} />
+            {/* 콘솔창 및 뿌옇게 표시하는 배경 */}
+            {showConsole && (
+        <div style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: 'rgba(255, 255, 255, 0.5)',
+          zIndex: 100003,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+          <div style={{
+            width: '80%',
+            height: '80%',
+            backgroundColor: '#333',
+            color: 'white',
+            padding: '20px',
+            overflowY: 'scroll',
+          }}>
+            Console Content
+          </div>
+        </div>
+      )}
     </div>
   );
 };
