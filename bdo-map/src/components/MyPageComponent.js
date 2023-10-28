019fb06472ac8cb2636f2d84ce1bf8e743b52135
Mackeabit <thdrudgh7775@naver.com>
2023-10-28 19:48:12 +0900

    ProjectComponent CSS 수정

--- bdo-map/src/components/MyPageComponent.js
+++ bdo-map/src/components/MyPageComponent.js
@@ -1,9 +1,8 @@
-// MyPageComponent.js
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import Sidebar from './SideBar';
-import ProjectComponent from './ProjectComponent';  // 추가된 import
+import ProjectComponent from './ProjectComponent';
 import './MyPageComponent.css';
 
 const MyPageComponent = () => {
@@ -11,10 +10,10 @@ const MyPageComponent = () => {
   const navigate = useNavigate();
   const [expanded, setExpanded] = useState(false);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [selectedProject, setSelectedProject] = useState(null);
 
   useEffect(() => {
-    if (!isAuthReady) return;  // 인증 상태가 준비되지 않았으면 아무 것도 하지 않음
-
+    if (!isAuthReady) return;
     if (!auth.isAuthenticated) {
       navigate('/login');
       return;
@@ -24,44 +23,72 @@ const MyPageComponent = () => {
     }, 100);
   }, [auth, navigate, isAuthReady]);
 
+  const handleProjectClick = (key) => {
+    setSelectedProject(key);
+  };
+
   return (
     <div className="container">
       <Sidebar isOpen={isSidebarOpen} toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} />
-      <div className={`card ${expanded ? 'expanded' : ''}`}>
-        <h2 className="title">
-          My Page
-        </h2>
+      <div className={`card ${expanded ? 'expanded' : ''} ${selectedProject ? 'shift-left' : ''}`}>
+        <h2 className="title">My Page</h2>
         <p>Welcome to your personal page!</p>
-        <ProjectComponent
-          key="1"
-          animationDelay="1.3s"
-          title="Healthy" 
-          description="Project about new app of company" 
-          role="Manager" 
-          time="1 week" 
-          progress={60} 
-        />
-        <ProjectComponent 
-          key="2"
-          animationDelay="1.6s"
-          title="Markers" 
-          description="Project about new app of company" 
-          role="Manager" 
-          time="1 week" 
-          progress={60} 
-        />
-        <ProjectComponent 
-          animationDelay="1.9s"
-          title="User" 
-          description="Project about new app of company" 
-          role="Manager" 
-          time="1 week" 
-          progress={60} 
-        />
+        <div className="project-list">
+          <ProjectComponent
+            key="1"
+            animationDelay="1.3s"
+            title="Healthy"
+            description="Project about new app of company"
+            role="Manager"
+            time="1 week"
+            progress={60}
+            onClick={() => handleProjectClick('1')}
+            isSelected={selectedProject === '1'}
+          />
+          <ProjectComponent
+            key="2"
+            animationDelay="1.6s"
+            title="Markers"
+            description="Project about new app of company"
+            role="User"
+            time="1 week"
+            progress={60}
+            onClick={() => handleProjectClick('1')}
+            isSelected={selectedProject === '1'}
+          />
+          <ProjectComponent
+            key="3"
+            animationDelay="1.9s"
+            title="Info"
+            description="Project about new app of company"
+            role="Guest"
+            time="1 week"
+            progress={60}
+            onClick={() => handleProjectClick('1')}
+            isSelected={selectedProject === '1'}
+          />
+          <ProjectComponent
+            key="4"
+            animationDelay="2.1s"
+            title="Data"
+            description="Project about new app of company"
+            role="Guest"
+            time="1 week"
+            progress={60}
+            onClick={() => handleProjectClick('1')}
+            isSelected={selectedProject === '1'}
+          />
+
+          {/* 다른 ProjectComponent들도 추가... */}
+        </div>
+        {selectedProject && (
+          <div className="selected-project">
+            {/* 선택된 프로젝트를 중앙에 표시 */}
+          </div>
+        )}
       </div>
     </div>
-  );  
-
+  );
 };
 
 export default MyPageComponent;
--- bdo-map/src/components/ProjectComponent.js
+++ bdo-map/src/components/ProjectComponent.js
@@ -3,14 +3,19 @@ import styles from './ProjectComponent.module.css';
 
 function ProjectComponent(props) {
   return (
-      <div className={`${styles.container} project-component`} style={{ animationDelay: props.animationDelay }}>
-          <div className={styles.title}>{props.title}</div>
-          <div className={styles.description}>{props.description}</div>
-          <div className={styles.details}>
-              <div className={styles.label}>{props.role}</div>
-              <div className={styles.value}>{props.time}</div>
-          </div>
+    <div
+      className={`${styles.container} project-component ${props.isSelected ? styles.selected : ''}`}
+      style={{ animationDelay: props.animationDelay }}
+      onClick={props.onClick}
+    >
+      <div className={styles.title}>{props.title}</div>
+      <div className={styles.description}>{props.description}</div>
+      <div className={styles.details}>
+        <div className={styles.label}>{props.role}</div>
+        <div className={styles.value}>{props.time}</div>
       </div>
+    </div>
   );
 }
+
 export default ProjectComponent;
